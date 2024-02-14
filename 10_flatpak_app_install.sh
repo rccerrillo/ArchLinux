a65c55353b59414ff5d3382c0e0c1373214b9219
rccerrillo <123270788+rccerrillo@users.noreply.github.com>
2024-02-14 14:09:17 -0700

    updates
    Updated the paclists adding more software and created two basic scripts for Flathub apps and enabling systemd services (first time install)

---
+++ 10_flatpak_app_install.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+flatpak install flathub --noninteractive org.gnome.Calculator com.visualstudio.code com.brave.Browser com.github.Eloston.UngoogledChromium com.github.Eloston.UngoogledChromium.Codecs com.github.IsmaelMartinez.teams_for_linux io.gitlab.librewolf-community net.cozic.joplin_desktop one.ablaze.floorp org.fkoehler.KTailctl org.kde.isoimagewriter org.qownnotes.QOwnNotes us.zoom.Zoom -y
---
+++ 11_enable_services.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+systemctl enable avahi-daemon.service bluetooth.service haveged.service cups.service upower.service sddm.service NetworkManager.service fstrim.timer reflector.timer
+
+
