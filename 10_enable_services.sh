86e96a9c294c235c65e407805e3b0ee024248ec8
rccerrillo <123270788+rccerrillo@users.noreply.github.com>
2025-06-28 10:00:29 -0700

    Update 10_enable_services.sh

--- 10_enable_services.sh
+++ 10_enable_services.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-systemctl enable avahi-daemon.service bluetooth.service haveged.service cups.service upower.service sddm.service NetworkManager.service reflector.timer
+systemctl enable avahi-daemon.service bluetooth.service haveged.service cups.service power-profiles-daemon.service sddm.service NetworkManager.service reflector.timer
 
 
